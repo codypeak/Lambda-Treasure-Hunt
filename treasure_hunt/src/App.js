a595dc616b77bec5a804556e584fa7eb54475872
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2019-02-14 16:33:22 -0700

    debugging app

--- treasure_hunt/src/App.js
+++ treasure_hunt/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
-import IntervalTimer from 'react-interval-timer';
 
-import Map from './Components/Map';
+//import Map from './Components/Map';
 
 require("dotenv").config();
 
@@ -27,6 +26,7 @@ class App extends Component {
       messages: [], 
       opposite_directions: {'n': 's', 's':'n', 'e': 'w', 'w': 'e'}
     };
+    this.perambulate = this.perambulate.bind(this);
   }
 
   componentDidMount() {
@@ -49,11 +49,13 @@ class App extends Component {
       .then(response => {
         console.log(response)
         let currentRoom = (response.data.room_id);
+        console.log(currentRoom);
         let coordinates = (response.data.coordinates);
         let x_coord = Number(coordinates.slice(1,3));
         let y_coord = Number(coordinates.slice(4,6));
         let graph = Object.assign({}, this.state.graph);
         let exits = response.data.exits;
+        console.log(exits)
         if (!(currentRoom in graph) ) {
           graph[currentRoom] = [x_coord, y_coord]
         }
@@ -71,8 +73,9 @@ class App extends Component {
             }
           }
           visited[currentRoom] = temp; //first time visit room update it with ?
+          console.log(visited);
         } 
-        this.setState({ ...this.state, currentRoom: response.data.room_id, exits: response.data.exits, title: response.data.title, graph: graph, description: response.data.description, visited: visited });
+        this.setState({ currentRoom: response.data.room_id, exits: response.data.exits, title: response.data.title, graph: graph, description: response.data.description, visited: visited });
       })
       .catch(err => console.log(err));
   };
@@ -86,7 +89,6 @@ class App extends Component {
         }   
     }
     const data = { direction: direction };
-    //let prevRoom = this.state.currentRoom;
   
     axios
       .post(moveUrl, data, reqOptions)
@@ -133,93 +135,103 @@ class App extends Component {
     localStorage.setItem(name, JSON.stringify(value));  //pass in a string name, then data being saved wil be value
   };  
   
-  cooldown_interval = () => {
-    setInterval(() => {
-      this.perambulate();
-      console.log('cooldown: ', this.state.currentRoom.cooldown);
-    }, 4000); //un-hard code this. set it to response data for penalties etc? 
+  cooldown_interval = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
   };
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
 
   route_finder = (currentRoom, path, graph) => {
     let directions = [];
     for (let i in path) {
-      next_room = path[i];
-      for (let direction in graph[this.state.currentRoom]) {
-        if (graph[this.state.currentRoom] === next_room) {
+      let next_room = path[i];
+      for (let direction in graph[currentRoom]) {
+        if (graph[currentRoom] === next_room) {
           directions.push(direction);
         }
       }
-      let currentRoom = visited[this.state.currentRoom][directions.length - 1];  
-      //return directions?
     }
+    return directions;
   };
 
   backtracker = currentRoom => {
     let queue = [];
     let visited = [];  // can we use visited from outside this function? or call this something else? 
-    let map = JSON.parse(localStorage.getItem('map'));
+    let graph = Object.assign({}, this.state.visited);
     queue.push([currentRoom]);
     while (queue.length > 0) {
       let path = queue.shift();
       console.log("path: ", path)
       let room_number = path[path.length - 1];
-      if (!visited.includes(room_number)) {
+      if (visited.includes(room_number) === false) {
         visited.push(room_number);
-        for (let exit of Object.entries(map[room_number])) {
+        for (let exit in graph[room_number]) {
           console.log('exit', exit);
-          if(map[room_number][exit] === "?") {
+          if(graph[room_number][exit] === "?") {
             return path;
           } else { 
             let duplicate_path = path.slice();  //slice adds shallow copy
-            duplicate_path.push(map[room_number][exit]);
+            duplicate_path.push(graph[room_number][exit]);
             queue.push(duplicate_path);
           }
         }
       };
     }
+    console.log('no path')
+    return []
   };
 
   direction_choices = currentRoom => {
-    if ('n' in visited[this.state.currentRoom] && visited[this.state.currentRoom]['n'] === "?") {
+    console.log(currentRoom);
+    console.log(this.state.exits);
+    if (this.state.exits.includes('n') && this.state.visited[currentRoom]['n'] === "?") {
       return 'n';
-    } else if ('s' in visited[this.state.currentRoom] && visited[this.state.currentRoom]['s'] === "?") {
+    } else if (this.state.exits.includes('s') && this.state.visited[currentRoom]['s'] === "?") {
       return 's';
-    } else if ('e' in visited[this.state.currentRoom] && visited[this.state.currentRoom]['e'] === "?") {
+    } else if (this.state.exits.includes('e') && this.state.visited[currentRoom]['e'] === "?") {
       return 'e';
-    } else if ('w' in visited[this.state.currentRoom] && visited[this.state.currentRoom]['w'] === "?") {
+    } else if (this.state.exits.includes('w') && this.state.visited[currentRoom]['w'] === "?") {
       return 'w';
+    } else {
+      return null;
     }
   };
 
-  perambulate = () => {
+  async perambulate() {
     let currentRoom = this.state.currentRoom;
-    console.log('Current room: ', currentRoom.room_id)
+    console.log('Current room: ', currentRoom)
     let backtrack = [];
     let rooms = [];
+    let unexplored_exit = null;
     let traversal_path = [];
-    while (visited.length < 500) {
+    while (Object.keys(this.state.visited).length < 500) {
+      const visited = Object.assign({}, this.state.visited);
+      currentRoom = this.state.currentRoom
       rooms.push(currentRoom);
       if (backtrack.length === 0) {
-        let unexplored_exit = direction_choices(currentRoom);
-          if (!unexplored_exit) {
-            let retrace_path = this.backtracker(currentRoom, visited);
-            let backtrack = this.route_finder()
-          } else {
-            traversal_path.append(unexplored_exit);
-            let next_room = graph[currentRoom.length - 1][unexplored_exit];  //this.state?  
-            let prev_direction = this.state.opposite_directions[unexplored_exit];
-            let next_room = visited[currentRoom][unexplored_exit];
-            let currentRoom = visited[next_room][prev_direction];
-            let currentRoom = next_room;
-          }
-      } else {
-        let unexplored_exit = this.backtrack.unshift();
-        //copy paste
+        unexplored_exit = this.direction_choices(currentRoom);
+        break;
       }
+
+      if (backtrack.length === 0 && unexplored_exit === null) {
+            console.log('start backtrack')
+            let retrace_path = this.backtracker(currentRoom, visited);
+            backtrack = this.route_finder(currentRoom, retrace_path, visited);
+      } 
+
+      if (backtrack.length > 0) {
+        console.log(backtrack)
+        unexplored_exit = backtrack.shift();
+        traversal_path.push(unexplored_exit);
+        this.move_request(unexplored_exit);  //response updates
+      } else {
+        traversal_path.push(unexplored_exit);
+        this.move_request(unexplored_exit);  
+      }
+
+      if (Object.keys(visited).length === 500) {
+        console.log(this.state.graph, this.state.visited);
+        break;
+      }
+      await this.cooldown_interval(this.state.cooldown*1200);
     };
   };
 
@@ -239,9 +251,9 @@ class App extends Component {
           <button onClick={() => this.move_request('e')}>East</button>
           <button onClick={() => this.move_request('w')}>West</button>
         </div>
-        {/* <div className="button-wrapper-2">
-          <button onClick={this.move_request}>Automatic Traversal</button>
-        </div> */}
+        <div className="button-wrapper-2">
+          <button onClick={this.perambulate}>Automatic Traversal</button>
+        </div>
         <div className="room-info">
           <h3>You are here:</h3>
           <p>The room you are currently in is: {this.state.title} {this.state.room_id}</p>
